a2523a3a378fb0c2c2d2bce5dc0d2a3249f785ba
Pablo Martínez Vergara <pablomartiver7@gmail.com>
2022-10-27 18:53:20 +0200

    Update Lisa_Level.py

--- Lisa_Level/Lisa_Level.py
+++ Lisa_Level/Lisa_Level.py
@@ -1,2 +1,19 @@
-import 
-import
+import shutil
+import requests
+import time
+import urllib.request
+import os
+import json
+
+while True:
+    time.sleep (30)
+    respuesta = requests.get ('https://thesimpsonsquoteapi.glitch.me/quotes')
+    respuesta.json()
+    frase=respuesta.json()[0]['quote']
+    personaje=respuesta.json()[0]['character']
+    imagen=respuesta.json()[0]['image']
+    frase_personaje = ['frase':frase,'personaje':personaje]
+    if not (os.path.exists(personaje)):
+        r=urllib.request.urlopen (imagen)
+        
+
